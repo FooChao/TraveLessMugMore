cc89fedea1f508c36ca7e0095f4e003f2b10eebe
FooChao <foochao01@gmail.com>
2024-07-07 18:00:09 +0800

    Twenty-Eighth Commit : Changed Credit

--- src/pages/CreditsPage.jsx
+++ src/pages/CreditsPage.jsx
@@ -6,7 +6,7 @@ const CreditsPage = () => {
         
         <p className="text-3xl mb-10">This website is made by Foo Chao</p>
         <p className="text-3xl mb-10">Any problems? Report at</p>
-        <a className="text-3xl mb-10 underline text-red-500 hover:text-red-700" href='https://github.com/FooChao/Test-Website' target='_blank'>https://github.com/FooChao/Test-Website</a>
+        <a className="text-3xl mb-10 underline text-red-500 hover:text-red-700" href='https://github.com/FooChao/TraveLessMugMore' target='_blank'>https://github.com/FooChao/TraveLessMugMore</a>
         
     </div>
   )
