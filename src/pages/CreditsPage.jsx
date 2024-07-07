a0490184ebd15df5e1121e3ca9ba973e345e5ff8
FooChao <foochao01@gmail.com>
2024-07-07 20:55:06 +0800

    Thirty-First Commit: Updated Credit

--- src/pages/CreditsPage.jsx
+++ src/pages/CreditsPage.jsx
@@ -5,9 +5,10 @@ const CreditsPage = () => {
     <div className="container text-3xl mx-auto">
         
         <p className="text-3xl mb-10">This website is made by Foo Chao</p>
-        <p className="text-3xl mb-10">Any problems? Report at</p>
+        <p className="text-3xl mb-10">Any problems with frontend? Report at</p>
         <a className="text-3xl mb-10 underline text-red-500 hover:text-red-700" href='https://github.com/FooChao/TraveLessMugMore' target='_blank'>https://github.com/FooChao/TraveLessMugMore</a>
-        
+        <p className="text-3xl mb-10">APIs are fetched from NUS Mod API. </p>
+               
     </div>
   )
 }
